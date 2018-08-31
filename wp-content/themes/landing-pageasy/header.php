ffb60cb98080aba1c321d5c23231127e4a4f101c
Fernando Mangas <fernandomangasmolteni@gmail.com>
2018-08-31 16:43:24 +0200

    Subo login

--- wp-content/themes/landing-pageasy/header.php
+++ wp-content/themes/landing-pageasy/header.php
@@ -56,23 +56,7 @@
 			</nav>
 		</div>
 	</div>  -->          
-<article class="article">
-			<div id="content_box" >
-				<?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
-					<div id="post-<?php the_ID(); ?>" <?php post_class('g post'); ?>>
-						<div class="single_page single_post clear">
-							<div id="content" class="post-single-content box mark-links">
-							<header>
-								<h1 class="title"><?php the_title(); ?></h1>
-							</header>
-								<?php the_content(); ?>                                    
-							</div><!--.post-content box mark-links-->
-							<?php comments_template( '', true ); ?>
-						</div>
-					</div>
-				<?php endwhile; ?>
-			</div>
-		</article>
+
 	<div class="container clear">
 		<div class="site-branding">
 			<div class="site-title">

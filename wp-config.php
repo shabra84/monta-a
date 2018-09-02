aad92f0d3c55bbef0fc2f9480f72a82993f4721e
118438 <u94561898@cbi-bs1.server.lan>
2018-09-02 04:18:52 +0200

    Subo modulos

--- wp-config.php
+++ wp-config.php
@@ -20,16 +20,16 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'montania');
+define('DB_NAME', 'db751798268');
 
 /** MySQL database username */
-define('DB_USER', 'root');
+define('DB_USER', 'dbo751798268');
 
 /** MySQL database password */
-define('DB_PASSWORD', '');
+define('DB_PASSWORD', '@Chica2018');
 
 /** MySQL hostname */
-define('DB_HOST', 'localhost');
+define('DB_HOST', 'db751798268.db.1and1.com');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8mb4');
